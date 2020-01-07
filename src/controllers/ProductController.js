8bbeb29cc59b37ea2ad9d3186f6c7d4d89572ce9
Guilherme de Alvarenga <guilherme.alvarenga@bludata.com.br>
2020-01-07 13:29:20 -0300

    Criação de registro

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -8,9 +8,10 @@ module.exports = {
 
         return res.json(products);
     },
+
+    async store(req, res){
+        const product = await Product.create(req.body);
+
+        return res.json(product);
+    }
 }
-// Product.create({ 
-//     title: 'React Native',
-//     description: 'Build native apps with React',
-//     url: 'https://github.com/facebook/react-native'
-// });
